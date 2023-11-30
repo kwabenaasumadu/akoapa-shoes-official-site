1387b40a1dfacdaccf5ca4d7a7c2125804b9758e
Kwabena <kasumadu@tapnhop.com>
2023-11-30 08:36:19 -0800

    kl

--- src/pages/user_signout/index.js
+++ src/pages/user_signout/index.js
@@ -2,16 +2,34 @@ import React from "react";
 import styles from "../../styles/user_login.module.css";
 import { auth } from "../../../firebase.config";
 import { useRouter } from "next/router";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Index() {
   const router = useRouter();
   const handleLogout = async () => {
     try {
       await auth.signOut();
+      toast.success("Logged out successfully!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
       console.log("User logged out successfully!");
 
       router.push("/user_login");
     } catch (error) {
+      toast.error(error.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+      });
       console.error("Error logging out:", error.message);
     }
   };
@@ -27,6 +45,7 @@ function Index() {
           <button onClick={handleLogout}>Logout</button>
         </div>
       </div>
+      <ToastContainer />
     </>
   );
 }
